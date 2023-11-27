48917f7a698bda3ebbdd2308fd06c7185a2a5812
mark23z <150701923+mark23z@users.noreply.github.com>
2023-11-27 13:46:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+import { hello } from './helper.js'
+
 const btn = document.getElementById("detect");
 const stars = document.getElementById("stars");
+const reviewText = document.getElementById("reviewText");
+const busted = document.getElementById("busted");
 
+//Die Ladeanimation des Buttons 
 function buttonLoader(){
     if(btn.classList.contains("button--loading")){
         btn.classList.remove("button--loading");
@@ -10,19 +15,17 @@ function buttonLoader(){
 
 }
 }
-
-
 btn.addEventListener("click", buttonLoader);
 
-const busted = document.getElementById("busted");
 
+//Dem Status Label das "Real" oder "Fake" anhängen
 function changeStatus(){
     busted.classList.add("good")
 
 }
-
 btn.addEventListener("click", changeStatus);
 
+//Nur Zahlen von 1-5 in dem Star-Input erlauben
 function changeNumber(){
     if(stars.value > 5){
         stars.value = null;
@@ -31,5 +34,41 @@ function changeNumber(){
     }
     console.log("hallo");
 }
-
 stars.addEventListener("keyup", changeNumber);
+
+//Den Button aktivieren, wenn überall der Input erfüllt ist
+function enableButton(){
+    if(stars.value != "" && stars.value != ""){
+        btn.disabled = false;
+    }else{
+        btn.disabled = true;
+    }
+
+}
+stars.addEventListener("change", enableButton);
+
+//Den Button aktivieren, wenn überall der Input erfüllt ist
+function enableField(){
+    if(reviewText.value != "" && stars.value != ""){
+        btn.disabled = false;
+    }else{
+        btn.disabled = true;
+    }
+
+}
+reviewText.addEventListener("change", enableField);               
+
+
+//ab hier Kommunikation mit Python ermöglichen
+const spawn = require('child_process');
+//für string:
+const text_to_pass_in = reviewText.value;
+const stars_to_pass_in = stars.value;
+
+console.log("Data sent to python:", text_to_pass_in);
+const python_process = spawn('python', ['C:\Users\Mark\Desktop\reviewDetector-main\python\test.ipynb',
+ text_to_pass_in, stars_to_pass_in]);
+
+python_process.stdout.on('data', (data) => {
+    console.log('Data received', data.toString());
+});
